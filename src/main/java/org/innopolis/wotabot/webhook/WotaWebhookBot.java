cd35c16a01d5d261184662d5f994f3c118326c7d
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-28 14:06:56 +0300

    logger was added to WotaWebhookBot

--- src/main/java/org/innopolis/wotabot/webhook/WotaWebhookBot.java
+++ src/main/java/org/innopolis/wotabot/webhook/WotaWebhookBot.java
@@ -1,11 +1,12 @@
 package org.innopolis.wotabot.webhook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.innopolis.wotabot.BotConfig;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-
+@Slf4j
 public class WotaWebhookBot extends TelegramWebhookBot {
 
     @Override
@@ -20,12 +21,12 @@ public class WotaWebhookBot extends TelegramWebhookBot {
 
     @Override
     public BotApiMethod onWebhookUpdateReceived(Update update) {
+        log.info("Bot got a request");
         if (update.hasMessage() && !update.getMessage().getText().isEmpty()) {
             SendMessage message = new SendMessage();
             message.setText("Well, I got a message");
             message.setChatId(update.getMessage().getChatId().toString());
             return message;
-
         }
         return null;
     }
