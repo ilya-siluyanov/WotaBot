ac312af3a863e1bd556d200f7d3c3aeb5fe25b32
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-29 01:08:12 +0300

    EntityManager bean was added

--- src/main/java/org/innopolis/wotabot/WotaBotApplication.java
+++ src/main/java/org/innopolis/wotabot/WotaBotApplication.java
@@ -4,7 +4,9 @@ import org.innopolis.wotabot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
@@ -14,6 +16,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 @SpringBootApplication
 @EnableJpaRepositories({"org.innopolis.wotabot.database"})
 @EntityScan("org.innopolis.wotabot.models")
+
 public class WotaBotApplication {
 
 
@@ -31,6 +34,13 @@ public class WotaBotApplication {
                 BotConfig.BOT_REDIRECT_URL);
         botsApi = new TelegramBotsApi(DefaultBotSession.class);
         botsApi.registerBot(webhookBot, new SetWebhook(url));
+    }
+
+    @Bean(name = "entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+        return sessionFactory;
     }
 
 }
