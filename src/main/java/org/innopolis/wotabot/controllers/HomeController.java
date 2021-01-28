e15038296f442e082f63fad14a980bab1d4996b4
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-28 14:02:14 +0300

    logger was added

--- src/main/java/org/innopolis/wotabot/controllers/HomeController.java
+++ src/main/java/org/innopolis/wotabot/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package org.innopolis.wotabot.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@Slf4j
 public class HomeController {
     @GetMapping
     public String homePage() {
@@ -12,7 +15,8 @@ public class HomeController {
     }
 
     @PostMapping
-    public String post() {
+    public String post(@RequestBody String req) {
+        log.info(req);
         return "home";
     }
 }
