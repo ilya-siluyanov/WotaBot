5d0ef3b48d0e0df70a2207cb257e2d192e0a8881
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-28 12:09:45 +0300

    First

---
+++ src/main/java/org/innopolis/wotabot/WotaBotApplication.java
@@ -0,0 +1,13 @@
+package org.innopolis.wotabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WotaBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WotaBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/innopolis/wotabot/WotaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.innopolis.wotabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WotaBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
