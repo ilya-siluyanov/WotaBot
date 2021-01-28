08f5dce689f8efa36ad6406283306bcc3e4081c3
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-29 01:00:14 +0300

    @EnableJPARepositories and @EntityScan annotations were added

--- src/main/java/org/innopolis/wotabot/WotaBotApplication.java
+++ src/main/java/org/innopolis/wotabot/WotaBotApplication.java
@@ -3,7 +3,8 @@ package org.innopolis.wotabot;
 import org.innopolis.wotabot.config.BotConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.updates.SetWebhook;
@@ -11,13 +12,15 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
+@EnableJpaRepositories({"org.innopolis.wotabot.database"})
+@EntityScan("org.innopolis.wotabot.models")
 public class WotaBotApplication {
 
 
     static TelegramWebhookBot webhookBot;
     static TelegramBotsApi botsApi;
 
-    public WotaBotApplication(TelegramWebhookBot webhookBot){
+    public WotaBotApplication(TelegramWebhookBot webhookBot) {
         WotaBotApplication.webhookBot = webhookBot;
     }
 
--- src/main/java/org/innopolis/wotabot/database/UserRepository.java
+++ src/main/java/org/innopolis/wotabot/database/UserRepository.java
@@ -3,8 +3,9 @@ package org.innopolis.wotabot.database;
 import org.innopolis.wotabot.models.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+@Repository
 public interface UserRepository extends CrudRepository<User, String> {
 
 }
