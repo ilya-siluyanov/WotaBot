b41f7d6dab036f8088e0d923440fc950b6fabda9
ilya-siluyanov <isiluyanov8@gmail.com>
2021-01-28 17:19:39 +0300

    bug fixed

--- src/main/java/org/innopolis/wotabot/controllers/MainController.java
+++ src/main/java/org/innopolis/wotabot/controllers/MainController.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 
-import java.util.Date;
-
 @Controller
 @Slf4j
 public class MainController {
@@ -25,23 +23,8 @@ public class MainController {
     @PostMapping
     public String post(@RequestBody String req) {
         JSONObject jsonObject = new JSONObject(req);
-        println(jsonObject);
+        System.out.println(jsonObject.toString());
         return "home";
     }
 
-
-    void println(JSONObject obj) {
-        JSONObject messageInfo = obj.getJSONObject("message");
-        JSONObject chatInfo = obj.getJSONObject("chat");
-        Date sent = new Date(obj.getLong("date"));
-        String text = obj.getString("text");
-
-        StringBuilder sb = new StringBuilder();
-        sb.append("{\n");
-        sb.append(messageInfo.toString());
-        sb.append(chatInfo.toString());
-        sb.append("}");
-        System.out.println(sb.toString());
-
-    }
 }
