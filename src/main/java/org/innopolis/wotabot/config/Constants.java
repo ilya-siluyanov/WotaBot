28ec9330b02b24bce546a2456b94d9d1de9c8aac
ilyasiluyanov <isiluyanov8@gmail.com>
2021-02-27 19:37:24 +0300

    bugs were fixed

--- src/main/java/org/innopolis/wotabot/config/Constants.java
+++ src/main/java/org/innopolis/wotabot/config/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
     public static final String SEND_MESSAGE = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s";
 
     public static boolean isWorkHasStarted = false;
-    public static boolean isWorkDone = true;
+    public static boolean isWorkDone = false;
     public static final String workIsDone = "Хозяин закончил надо мной работать.";
     public static final String workIsInProgress = "Хозяин начал работать надо мной. Извините, если будет много лишних сообщений";
 
